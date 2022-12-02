2755d351d2623a7ab300a2568486c898c8457d46
Ethan Matthews <ethanlanematthews@gmail.com>
2022-12-02 09:35:53 -0500

    Updated comments

--- test.py
+++ test.py
@@ -1,20 +1,30 @@
+"""
+Name: Ethan Matthews
+Project: Inverted Pendulum ODE simulation
+Description: Project for Differentials Equations class
+in which we need to model an inverted pendulum
+"""
+
 from matplotlib import pyplot as plt
 from scipy.integrate import odeint
 import numpy as np
 
+# Constants given by the project
 g = 9.8
 L = 2
 c = 0.25
 
+# Function that holds the basic ODE
 def u(u, t):
     return (u[1],-c*u[1]-(g/L)*np.sin(u[0]))
 
+# Setting up the graphing for the ODE
 y0 = [0.01,0]
 ts = np.linspace(1,40,1000)
 us = odeint(u, y0, ts)
 ys = us[:,0]
 plt.plot(ts,ys,'-')
-#plt.plot(ts,ys,'r*')
+
 plt.xlabel('t values')
 plt.ylabel('u values')
 plt.title('Solution')
